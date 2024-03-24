af695b13fea185e4c7c6f550599780e6289d3078
SamWaku <56162251+SamWaku@users.noreply.github.com>
2024-03-24 05:03:53 +0200

    local

---
+++ Screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>HomeScreen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
