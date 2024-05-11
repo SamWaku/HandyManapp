bf545c012918644ec0439a0e9e6510d4dba37058
SamWaku <56162251+SamWaku@users.noreply.github.com>
2024-05-11 22:32:17 +0200

    homescreen

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text, SafeAreaView } from "react-native";
+import AppBar from "../components/AppBar";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
+      {/* <AppBar /> */}
       <View>
-        <Text>HomeScreen</Text>
+        <Text className=" text-red-600">HoomeScreen</Text>
       </View>
     </SafeAreaView>
   );
---
+++ components/AppBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const AppBar = (props: Props) => {
+  return <div>AppBar</div>;
+};
+
+export default AppBar;
