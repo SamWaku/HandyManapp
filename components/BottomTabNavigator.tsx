ba2ab8f1c3569acac82d207547d46c9912e41904
SamWaku <56162251+SamWaku@users.noreply.github.com>
2024-05-11 23:15:30 +0200

    homescreen

---
+++ components/BottomTabNavigator.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const BottomTabNavigator = () => {
+  return <div>BottomTabNavigator</div>;
+};
+
+export default BottomTabNavigator;
